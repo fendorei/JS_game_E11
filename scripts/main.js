0f595b0730c053fa27df7d564fd46381bd6b8095
Kilian Marechal <kilian.marechal@hetic.net>
2019-04-16 15:31:19 +0200

    Level1 separated js file, door, additional platforms, "game" to "traps"

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,14 @@
-let img = new Image();   // Crée un nouvel élément img
-img.addEventListener('load', function() {
-//  exécute les instructions drawImage ici
+let imgPlayer = new Image();
+imgPlayer.addEventListener('load', function() {
+//  executes drawImage instructions here
 }, false);
+imgPlayer.src = 'images/hero_left_0.gif'
 
-img.src = 'images/hero_left_0.gif'
+let imgDoor = new Image();
+imgDoor.addEventListener('load', function() {
+//  executes drawImage instructions here
+}, false);
+imgDoor.src = 'images/openedDoor_lv1.png'
 
 let canvas = document.querySelector('canvas'),
     context = canvas.getContext('2d'),
@@ -21,72 +26,22 @@ let canvas = document.querySelector('canvas'),
         jumping: false,
         grounded: false
     },
-    
+
     keys = [],
     friction = 0.8,
     gravity = 0.45;
 
-//Borders and Platforms
-let boxes = [];
-//Game Left Border
-boxes.push({
-    x: 0,
-    y: 0,
-    width: 20,
-    height: height
-});
-//Game Right Border
-boxes.push({
-    x: width - 20,
-    y: 0,
-    width: 20,
-    height: height
-});
-//Game Floor
-boxes.push({
-    x: 0,
-    y: height - 10,
-    width: width,
-    height: 50
-});
-//Game Roof
-boxes.push({
-    x: 0,
-    y: 0,
-    width: width,
-    height: 10
-});
-//Beginning Platform Floor
-boxes.push({
-    x: 100,
-    y: 250,
-    width: 50,
-    height: 10
-});
-//Beginning Platform Right Wall
-boxes.push({
-    x: 148,
-    y: 225,
-    width: 10,
-    height: 35
-});
-//Middle-bottom Platform
-boxes.push({
-    x: 475,
-    y: 560,
-    width: 50,
-    height: 30  
-});
-//Right-bottom Platform
-boxes.push({
-    x: 800,
-    y: 520,
-    width: 20,
-    height: 80
-});
-
-canvas.width = width;
-canvas.height = height;
+    class Door {
+        constructor(x, y, width, height) {
+            this.x = x
+            this.y = y
+            this.width = width
+            this.height = height
+        }
+        nextLevel() {
+            
+        }
+    }
 
 function update() {
     // check keys
@@ -115,9 +70,13 @@ function update() {
     player.velY += gravity;
 
     //Blocks
-    context.clearRect(0, 0, width, height);
-    context.fillStyle = "black";
     context.beginPath();
+    context.clearRect(0, 0, width, height);
+
+    //Door
+    context.beginPath();
+    context.fillRect(doorLevel1.x, doorLevel1.y, doorLevel1.width, doorLevel1.height)
+    context.drawImage(imgDoor,doorLevel1.x, doorLevel1.y, doorLevel1.width, doorLevel1.height)
 
     //Player interactions
     player.grounded = false;
@@ -147,7 +106,7 @@ function update() {
 
     //Player
     context.fill()
-    context.drawImage(img, player.x, player.y, player.width, player.height)
+    context.drawImage(imgPlayer, player.x, player.y, player.width, player.height)
 
     requestAnimationFrame(update);
 }
