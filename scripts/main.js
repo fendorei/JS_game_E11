1f78076dfd1084fe3e7c633a0c1b7660820b0059
Thibault LIMMOIS <thibault.limmois@gmail.com>
2019-04-17 15:54:36 +0200

    Timer

--- scripts/main.js
+++ scripts/main.js
@@ -255,3 +255,42 @@ setInterval(
     },
     100
 );
+
+(() => {
+
+  let hours = `00`,
+      minutes = `00`,
+      seconds = `00`,
+      chronometerDisplay = document.querySelector(`[data-chronometer]`),
+      chronometerCall
+
+  function chronometer() {
+
+    seconds ++
+
+    if (seconds < 10) seconds = `0` + seconds
+
+    if (seconds > 59) {
+      seconds = `00`
+      minutes ++
+
+      if (minutes < 10) minutes = `0` + minutes
+    }
+
+    if (minutes > 59) {
+      minutes = `00`
+      hours ++
+
+      if (hours < 10) hours = `0` + hours
+    }
+
+    chronometerDisplay.textContent = `${hours}:${minutes}:${seconds}`
+
+  }
+
+  play.onclick = (event) => {
+    chronometerCall = setInterval(chronometer, 1000)
+    event.target.setAttribute(`disabled`,``)
+  }
+
+})()
